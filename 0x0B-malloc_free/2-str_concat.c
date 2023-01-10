f769062002f8d3bf8dcf48a9c8269944f4b7c0e4
Joy-NN <joynyambura7890@gmail.com>
2023-01-10 03:59:16 -0800

    malloc files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,32 +21,25 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 
 		s2 = "";
-
 	i = ci = 0;
 
 	while (s1[i] != '\0')
 
 		i++;
-
 	while (s2[ci] != '\0')
 
 		ci++;
-
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
 
 		return (NULL);
-
 	i = ci = 0;
-
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
-
 			i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
@@ -54,6 +47,5 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	conct[i] = '\0';
-
 	return (conct);
 }
